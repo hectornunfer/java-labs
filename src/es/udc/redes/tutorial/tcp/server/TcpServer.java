ff930ffb0578dc685ad7923d2b7531c6c2f565d2
Héctor Núñez <hectornunfer@gmail.com>
2021-02-25 23:25:05 +0100

    Git Ignore

--- src/es/udc/redes/tutorial/tcp/server/TcpServer.java
+++ src/es/udc/redes/tutorial/tcp/server/TcpServer.java
@@ -1,4 +1,5 @@
 package es.udc.redes.tutorial.tcp.server;
+import java.io.IOException;
 import java.net.*;
 
 /** Multithread TCP echo server. */
@@ -6,17 +7,23 @@ import java.net.*;
 public class TcpServer {
 
   public static void main(String argv[]) {
-    if (argvlength != 1) {
+    if (argv.length != 1) {
       System.err.println("Format: es.udc.redes.tutorial.tcp.server.TcpServer <port>");
       System.exit(-1);
     }
+    ServerSocket serverSocket = null;
     try {
       // Create a server socket
+      serverSocket = new ServerSocket(Integer.parseInt(argv[0]));
       // Set a timeout of 300 secs
+      serverSocket.setSoTimeout(300000);
       while (true) {
         // Wait for connections
+        Socket socket = serverSocket.accept();
         // Create a ServerThread object, with the new connection as parameter
+        ServerThread st = new ServerThread(socket);
         // Initiate thread using the start() method
+        st.start();
       }
     } catch (SocketTimeoutException e) {
       System.err.println("Nothing received in 300 secs");
@@ -25,6 +32,11 @@ public class TcpServer {
       e.printStackTrace();
      } finally{
 	//Close the socket
+      try{if(serverSocket!=null) serverSocket.close();
+      }
+      catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
