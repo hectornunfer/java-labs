6fcad0e0d864aec4d2b0f1a09b47573d37541841
Héctor Núñez <hectornunfer@gmail.com>
2021-02-25 23:33:37 +0100

    Copy

--- src/es/udc/redes/tutorial/copy/Copy.java
+++ src/es/udc/redes/tutorial/copy/Copy.java
@@ -1,9 +1,32 @@
 package es.udc.redes.tutorial.copy;
 
+import java.io.*;
+
 public class Copy {
+    public Copy(String sourceFile, String destinationFile) throws IOException{
+        BufferedInputStream inputStream = null;
+        BufferedOutputStream outputStream = null;
 
-    public static void main(String[] args) {
+        try {
+            inputStream = new BufferedInputStream(new FileInputStream(sourceFile));
+            outputStream = new BufferedOutputStream(new FileOutputStream(destinationFile));
+            int c;
 
+            while ((c = inputStream.read()) != -1) {
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
     }
-    
+    public static void main(String[] args) throws IOException {
+        if(args.length == 2) {
+            new Copy(args[0], args[1]);
+        } else System.out.println("Error de parámetros.");
+    }
 }
